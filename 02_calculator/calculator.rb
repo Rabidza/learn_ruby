2f6eae95321a0b7f54fe1447c2bbbbd7a53efaa7
Neill Henning <neillhenning@gmail.com>
2019-05-20 20:06:41 +0200

    Started with exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
-#write your code here
+def add num1, num2
+  num1 + num2
+end
+
+def subtract num1, num2
+  num1 - num2
+end
+
+def sum arr
+  total = 0
+  for num in arr
+    total += num
+  end
+  return total
+end
+
+def multiply num1, num2
+  num1 * num2
+end
+
+def power num1, num2
+  num1 ** num2
+end
+
+def factorial num
+end
