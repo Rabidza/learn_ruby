70610180b1951a1d74e96a06bd859a583e6a6af6
Neill Henning <neillhenning@gmail.com>
2019-05-21 11:34:22 +0200

    Finish Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo string
+  string
+end
+
+def shout string
+  string.upcase
+end
+
+def repeat string, num=2
+  string + (" #{string}" * (num - 1))
+end
+
+def start_of_word string, num
+  string.slice(0, num)
+end
+
+def first_word string
+  string.split.first
+end
+
+def titleize string
+  words = string.split
+  result = words[0].capitalize
+  words.shift
+  little_words = ["and", "over", "the"]
+  words.each do |word|
+    if little_words.include?(word)
+      result += " " + word
+    else
+      result += " " + word.capitalize
+    end
+  end
+  result
+end
