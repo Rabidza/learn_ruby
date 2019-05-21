6f4204fab045b92cf0ddf8726acadb8486199186
Neill Henning <neillhenning@gmail.com>
2019-05-21 16:12:03 +0200

    Finish Book Titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_accessor :title
+  
+  def title
+    little_words = ["and", "in", "the", "of", "a", "an"]
+    @title = @title.split.map do |word|
+      if little_words.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    @title[0].capitalize!
+    @title.join(" ")
+  end
 end
