70d92b251ea53a998e1ae5c97c5121d607f34172
Neill Henning <neillhenning@gmail.com>
2019-05-21 13:28:08 +0200

    Finish pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
-#write your code here
+def translate string
+  words = string.split(' ')
+  words = words.map do |word|
+    if word.index('qu')
+      index = word.index('qu')
+      parts = [word.slice(0..index+1), word.slice(index+2..)]
+    else 
+       parts = word.split(/([aeiou].*)/)     
+    end
+    parts[1] + parts[0] + "ay"
+  end
+  words.join(' ')
+end
